d0d95edd8601a051cdc9033eb5818ff8a5992d30
amandaveredas <amandaveredas@gmail.com>
2022-02-07 10:51:13 -0300

    FEAT: busca elemento do vetor

--- src/com/amanda/estruturadados/vetor/Main.java
+++ src/com/amanda/estruturadados/vetor/Main.java
@@ -2,7 +2,15 @@ package com.amanda.estruturadados.vetor;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
-        vetor.adiciona("elemento");
+
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor.getTamanho());
+        System.out.println(vetor);
+        System.out.println(vetor.busca(0));
     }
 }
