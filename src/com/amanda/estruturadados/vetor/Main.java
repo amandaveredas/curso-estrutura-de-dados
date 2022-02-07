f827a061ecf05c9a899d9e2f625da57ae8219c23
amandaveredas <amandaveredas@gmail.com>
2022-02-07 14:00:42 -0300

    FEAT: Cria classe Contato.

--- src/com/amanda/estruturadados/vetor/Main.java
+++ src/com/amanda/estruturadados/vetor/Main.java
@@ -35,9 +35,24 @@ public class Main {
         System.out.println(vetorObjetos);
         System.out.println(vetorObjetos.getTamanho());
 
+        Contato contato1 = new Contato("Amanda","233333336","amanda@amanda.com.br");
+        Contato contato2 = new Contato("Roni","233333336","roni@amanda.com.br");
+        Contato contato3 = new Contato("Marluce","233333336","marluce@amanda.com.br");
+        Contato contato4 = new Contato("Amanda","233333336","amanda@amanda.com.br");
 
+        VetorObjetos contatos = new VetorObjetos(3);
+        contatos.adiciona(contato1);
+        contatos.adiciona(contato2);
+        contatos.adiciona(contato3);
 
+        int pos = contatos.busca(contato4);
+        if (pos > -1){
+            System.out.println("O contato está na posição " + pos);
+        }else{
+            System.out.println("Elemento não encontrado!");
+        }
 
+        System.out.println(contatos);
 
 
 
