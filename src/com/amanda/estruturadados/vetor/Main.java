7b1e533f946bbc9ef762c7b300fccc5d63a2c81e
amandaveredas <amandaveredas@gmail.com>
2022-02-07 10:31:52 -0300

    FEAT: Adiciona elemento no vetor

---
+++ src/com/amanda/estruturadados/vetor/Main.java
@@ -0,0 +1,8 @@
+package com.amanda.estruturadados.vetor;
+
+public class Main {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+        vetor.adiciona("elemento");
+    }
+}
