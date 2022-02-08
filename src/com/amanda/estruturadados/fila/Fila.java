41cbf6ea0dc646c3b53e8f71d0b28453c23f4963
amandaveredas <amandaveredas@gmail.com>
2022-02-08 16:29:10 -0300

    FEAT: Cria classe Fila e os métodos enfileira e espia

---
+++ src/com/amanda/estruturadados/fila/Fila.java
@@ -0,0 +1,26 @@
+package com.amanda.estruturadados.fila;
+
+import com.amanda.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T> {
+
+    public Fila() {
+        super();
+    }
+
+    public Fila(int capacidade) {
+        super(capacidade);
+    }
+
+    public void enfileira(T elemento){
+        this.adiciona(elemento);
+    }
+
+    public T espia(){
+        return this.elementos[0];
+    }
+
+
+
+
+}
---
+++ src/com/amanda/estruturadados/fila/UsandoAFila.java
@@ -0,0 +1,16 @@
+package com.amanda.estruturadados.fila;
+
+public class UsandoAFila {
+    public static void main(String[] args) {
+        Fila<Integer> fila = new Fila<>();
+
+        System.out.println(fila.espia());
+
+        fila.enfileira(1);
+        fila.enfileira(2);
+        fila.enfileira(3);
+
+        System.out.println(fila);
+        System.out.println(fila.espia());
+    }
+}
