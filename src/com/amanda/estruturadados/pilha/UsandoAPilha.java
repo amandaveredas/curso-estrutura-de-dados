11e9781de302b5a9f74f754fc5dbf913325a9199
amandaveredas <amandaveredas@gmail.com>
2022-02-08 14:40:06 -0300

    FEAT: Implementa método empilha na classe Pilha

--- src/com/amanda/estruturadados/pilha/UsandoAPilha.java
+++ src/com/amanda/estruturadados/pilha/UsandoAPilha.java
@@ -2,9 +2,14 @@ package com.amanda.estruturadados.pilha;
 
 public class UsandoAPilha {
     public static void main(String[] args) {
-        Pilha<String> pilha = new Pilha<>();
+        Pilha<Integer> pilha = new Pilha<>();
 
         System.out.println(pilha);
         System.out.println(pilha.getTamanho());
+
+        for(int i=1;i<=11;i++){
+            pilha.empilha(i);
+        }
+        System.out.println(pilha);
     }
 }
